3af886685ba440377f1d3baf5e479f6b599307ea
Pavan <pavankumarpai@gmail.com>
2025-04-11 07:45:43 -0400

    Update Head.tsx

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -2,7 +2,7 @@ import NextHead from "next/head";
 import Script from "next/script";
 import React from "react";
 
-const description = `Dhani is an open group project to restore genealogical connections and collect information about the origins of the Karkala Konkani family and their relationship with other families`;
+const description = `🙏 Karkala Dhani ✨ is an open group project to restore genealogical connections and collect information about the origins of the Karkala Konkani family and their relationship with other families`;
 
 export const PageHead = () => (
   <>
