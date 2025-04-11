14726066a05648f8e08dae52f3e4b101c2b49ba1
Pavan <pavankumarpai@gmail.com>
2025-04-11 07:51:42 -0400

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import s from "./Footer.module.css";
 const Footer: FC = () => (
   <footer className={s.footer}>
     <span className={s.footerItem}>Jai Shri Venkatramana! 🙏✨</span>
-    <span className={s.footerItem}>© 2025 <a hreh=\"https://karrot.tech/\">Karrot Tech (🥕📡)</a>. All rights reserved.</span>
+    <a hreh="https://karrot.tech/"><span className={s.footerItem}>© 2025 Karrot Tech (🥕📡). All rights reserved.</span></a>
   </footer>
 );
 
