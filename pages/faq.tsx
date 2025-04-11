ed4647c86075ef46240a35dc1ddccb213d97c78a
Pavan <pavankumarpai@gmail.com>
2025-04-11 07:17:19 -0400

    Update faq.tsx to english

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -12,41 +12,39 @@ import yellowCircle from "../public/yellowCircle.png";
 
 const questions = [
   {
-    question: "Если я есть в дереве, информацию обо мне можно будет найти в Гугл или Яндекс поисковике?",
+    question: "If I am in the tree, will information about me be available in Google or other search engines?",
     answer: (
       <span>
-        Нет, дерево не индексируется, поэтому никто не найдет этот сайт, задав в строке поиска ваше ФИО. Но если вам
-        беспокойно от того, что ваши данные есть в дереве,{" "}
+        No, the tree is not indexed, so no one will find this site by entering your full name in the search bar. But if you are worried about your data being in the tree,{" "}
         <BioLink
-          href="https://wa.me/+79853522893?text=Здравствуйте!%20 Пишу%20насчет%20проекта%20ДРЕВО"
-          text="напишите нам в WhatsApp"
+          href="https://wa.me/+12242292932?text=Hello%21%20I%20am%20writing%20regarding%20the%20Dhani%20-%20Tree%20project"
+          text="Contact us on WhatsApp"
           newTab={true}
         />{" "}
-        - удалим вас из дерева или сократим информацию до комфортного минимума.
+        - we will remove you from the tree or reduce the information to a comfortable minimum.
       </span>
     ),
   },
   {
-    question: "Хочу добавить/удалить/исправить информацию. Как это сделать?",
+    question: "I want to add/delete/correct information. How do I do this?",
     answer: (
       <BioLink
-        href="https://wa.me/+79853522893?text=Здравствуйте!%20 Пишу%20насчет%20проекта%20ДРЕВО"
-        text="Напишите нам в WhatsApp."
+        href="https://wa.me/+12242292932?text=Hello%21%20I%20am%20writing%20regarding%20the%20Dhani%20-%20Tree%20project"
+        text="Contact us on WhatsApp"
         newTab={true}
       />
     ),
   },
   {
-    question: "Информация о каких семьях уже есть в проекте?",
+    question: "What families already have information about them in the project?",
     answer: (
       <span>
-        На <BioLink href="/families" text="странице Семьи" newTab={true} /> представлен полный список доступных на
-        данных момент семей. Каждая фамилия в этом списке - это ссылка, по которой можно перейти и посмотреть дерево
-        этой семьи. Некоторые деревья большие, некоторые - еще маленькие. Если вдруг вы желаете помочь нам и дополнить
-        дерево,{" "}
+        На <BioLink href="/families" text="Family page" newTab={true} /> a complete list of families available at the moment is presented. 
+        Each family name in this list is a link that you can follow to view the tree of this family. 
+        Some trees are large, some are still small. If you suddenly want to help us and add to the tree,{" "}
         <BioLink
-          href="https://wa.me/+79853522893?text=Здравствуйте!%20 Пишу%20насчет%20проекта%20ДРЕВО"
-          text="напишите нам в WhatsApp"
+          href="https://wa.me/+12242292932?text=Hello%21%20I%20am%20writing%20regarding%20the%20Dhani%20-%20Tree%20project"
+          text="Contact us on WhatsApp"
           newTab={true}
         />
         .
@@ -54,23 +52,22 @@ const questions = [
     ),
   },
   {
-    question: "Что я вижу на странице Дерево?",
+    question: "What do I see on the Tree page?",
     answer: (
       <span>
-        На <BioLink href="/tree" text="странице Дерево" newTab={true} /> можно посмотреть информацию о представителях
-        той или иной семьи и виде дерева. Дерево состоит из узлов в виде шариков, каждый узел - это отдельный человек.
-        Дерево строится сверху вниз, то есть старшие поколения расположены выше. Дети расположены слева направо - от
-        старшего к младшему.
+        At <BioLink href="/tree" text="Tree page" newTab={true} /> you can see information about the representatives of a particular family and the type of tree. 
+        The tree consists of nodes in the form of balls, each node is a separate person. 
+        The tree is built from top to bottom, that is, the older generations are located above. 
+        Children are located from left to right - from the eldest to the youngest.
         <br />
         <br />
         <div className={s.pictureWrapper}>
           <Image src={tree} layout="responsive" />
         </div>
         <br />
-        По умолчанию показано дерево семьи Артемовых, но там также можно посмотреть деревья и для других семей. На{" "}
-        <BioLink href="/families" text="странице Семьи" newTab={true} /> можно посмотреть список ссылок на деревья
-        других семей. А на карточке с подробной информацией о человеке есть вкладка Семья со списком тех семей, потомком
-        которых является этот человек.
+        By default, the Karkala Pai family tree is shown, but you can also see trees for other families.{" "}
+        <BioLink href="/families" text="Family page" newTab={true} />you can see a list of links to trees of other families. 
+        And on the card with detailed information about a person there is a Family tab with a list of those families of which this person is a descendant.
         <br />
         <br />
         <div className={s.pictureWrapper}>
@@ -80,54 +77,50 @@ const questions = [
     ),
   },
   {
-    question: "Что я могу узнать о конкретном человеке? Где это посмотреть?",
+    question: "What can I find out about a specific person? Where can I see it?",
     answer: (
       <span>
-        На <BioLink href="/tree" text="странице Дерево" newTab={true} /> можно посмотреть дерево семьи. Дерево состоит
-        из узлов в виде шариков. На каждом шарике отображается ФИО человека и его годы жизни, если они нам известны.
+        At <BioLink href="/tree" text="Tree page" newTab={true} /> you can see the family tree. The tree consists of nodes in the form of balls. 
+        Each ball displays the person's full name and years of life, if we know them.
         <br />
         <br />
         <div className={s.pictureWrapper}>
           <Image src={tree} layout="responsive" />
         </div>
         <br />
-        Также по клику на шарик открывается карточка с подробной информацией о человеке, где вы можете видеть 3 вкладки:
-        Биография, Галерея и Семьи.
+        Also, by clicking on the ball, a card with detailed information about the person opens, where you can see 3 tabs: 
+        Biography, Gallery and Families.
         <br />
         <br />
         <div className={s.pictureWrapper}>
           <Image src={personCard} layout="responsive" />
         </div>
         <br />
-        На вкладке Биография может быть информация о дате и месте рождения, дате и месте смерти, ближайших родственниках
-        человека (родители, супруги, дети, братья и сестры). Также там можно найти информацию о национальности,
-        образовании, профессии и наградах человека и его краткую биографию. К сожалению, мы не обладаем такой
-        информацией о всех людях в дереве, поэтому если вы желаете помочь и добавить что-то, напишите нам в WhatsApp
-        (ссылка).
+        The Biography tab may contain information about a person’s date and place of birth, date and place of death, and close relatives (parents, spouses, children, brothers and sisters). 
+        You can also find information about the person’s nationality, education, profession, awards, and a brief biography. 
+        Unfortunately, we do not have such information for all individuals in the tree, so if you would like to help and add something, please write to us on WhatsApp.
         <br />
         <br />
-        На вкладке Галерея можно посмотреть фотографии этого человека.
+        On the Gallery tab you can see photos of this person.
         <br />
         <br />
-        На вкладке Семьи можно увидеть список семей, потомком которых является этот человек. Каждая фамилия там - это
-        ссылка на дерево этой семьи.
+        On the Families tab you can see a list of families that this person is a descendant of. Each family name there is a link to the tree of this family.
       </span>
     ),
   },
   {
-    question: "Почему некоторые шарики имеют желтое кольцо?",
+    question: "Why do some balloons have a yellow ring?",
     answer: (
       <span>
-        Так мы отмечаем людей, чьи родители не отображаются в просматриваемом дереве. Мы специально не показываем
-        всех-всех в одном дереве, потому что такое дерево сложно не только построить, но и читать…
+        This is how we mark people whose parents are not shown in the tree being viewed. 
+        We deliberately do not show everyone in one tree, because such a tree is not only difficult to build, but also difficult to read…
         <br />
         <br />
         <div className={s.pictureWrapper}>
           <Image src={yellowCircle} layout="responsive" />
         </div>
         <br />
-        Но вы можете открыть карточку человека с таким шариком, перейти на вкладку Семьи и увидеть, потомком каких семей
-        он является. Каждая фамилия там - это ссылка на дерево этой семьи.
+        But you can open the card of a person with such a ball, go to the Families tab and see which families he is a descendant of. Each surname there is a link to the tree of this family.
         <br />
         <br />
         <div className={s.pictureWrapper}>
@@ -137,11 +130,10 @@ const questions = [
     ),
   },
   {
-    question: "Я могу поделиться ссылкой на конкретное дерево?",
+    question: "Can I share a link to a specific tree?",
     answer: (
       <span>
-        Конечно! По умолчанию всегда открывается дерево семьи Артемовых, но если вы откроете дерево другой семьи и
-        скопируете ссылку из строки браузера, то эта ссылка всегда будет вести на это дерево.
+        Of course! By default, the Karkala Pai family tree always opens, but if you open another family tree and copy the link from the browser line, this link will always lead to this tree.
       </span>
     ),
   },
@@ -159,13 +151,13 @@ const FaqPage: NextPage = () => (
         </div>
         <div className={classNames(s.subtitleContainer, s.descriptionItem)}>
           <span>
-            Не нашли ответ на свой вопрос?{" "}
+            Didn't find the answer to your question?{" "}
             <BioLink
-              href="https://wa.me/+79853522893?text=Здравствуйте!%20 Пишу%20насчет%20проекта%20ДРЕВО"
-              text="Напишите нам в WhatsApp!"
+              href="https://wa.me/+12242292932?text=Hello%21%20I%20am%20writing%20regarding%20the%20Dhani%20-%20Tree%20project"
+              text="Contact us on WhatsApp"
               newTab={true}
             />{" "}
-            Мы будем рады помочь, а также получить ценную обратную связь, чтобы сделать сайт удобнее и понятнее.
+            We will be happy to help and receive valuable feedback to make the site more convenient and understandable.
           </span>
         </div>
       </div>
