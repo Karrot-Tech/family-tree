7e422fb4d03664a53e4aabfa74060796a1c1d229
pavanpaik <pavankumarpai@gmail.com>
2025-04-13 12:18:49 -0400

    replacing &apos;

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const HomePage: NextPage = () => {
             Grace of 🙏 Karkala Dhani ✨ has given raise to 👨‍👩‍👧‍👦 Family Tree 🌳... a digital record of Karkala Vasudaiva Kutumbakam.
           </span>
           <span className={classNames(s.description, s.descriptionItem)}>
-            This open group project provides information about the origins of the Gaud Saraswat Brahmin's Pai family from the Town of Karkala, Udupi District, Karnataka, India (Bharatvarsha). 
+            This open group project provides information about the origins of the Gaud Saraswat Brahmin&apos;s Pai family from the Town of Karkala, Udupi District, Karnataka, India (Bharatvarsha). 
           </span>
           <span className={classNames(s.description, s.descriptionItem)}>
             This 🌳 also provides relationship of individuals from Karkala family with other families.
