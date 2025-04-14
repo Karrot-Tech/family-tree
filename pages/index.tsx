3025f60d97f0831b157061c80b827ecf00fa4ed2
pavanpaik <pavankumarpai@gmail.com>
2025-04-14 04:49:14 -0400

    vamsa vruksham

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const HomePage: NextPage = () => {
             <span className={s.logoTitle}>👨‍👩‍👧‍👦 Family Tree 🌳</span>
           </div>
           <span className={classNames(s.description, s.descriptionItem)}>
-            Grace of 🙏 Karkala Dhani ✨ has given raise to 👨‍👩‍👧‍👦 Family Tree 🌳 (Vaṁśa Vṛkṣam)... a digital record of Karkala Vasudaiva Kutumbakam.
+            Grace of 🙏 Karkala Dhani ✨ has given raise to 👨‍👩‍👧‍👦 Family Tree 🌳 (Vamsa Vruksham)... a digital record of Karkala Vasudaiva Kutumbakam.
           </span>
           <span className={classNames(s.description, s.descriptionItem)}>
             This open group project provides information about the origins of the Gaud Saraswat Brahmin&apos;s Pai family from the Town of Karkala (Padutirupati), Udupi District, Karnataka, India (Bharatvarsha). 
